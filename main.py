3d186f57a90b54912c46f03e768763b1fd95ebce
dz1domin <36998838+dz1domin@users.noreply.github.com>
2018-03-21 17:59:27 +0100

    more

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from tttBoard import tttBoard
 
-board = tttBoard()
-board.draw_board()
+# todo: handle inputs
+# todo: base game loop and player prompts
+# todo: randomizing who goes first and attributing win accordingly
 
-board.set_point(0, 0, 'X')
-board.set_point(0, 1, 'X')
+board = tttBoard()
+
+board.set_point(0, 0, ' ')
+board.set_point(0, 1, ' ')
 board.set_point(0, 2, 'X')
-board.set_point(1, 0, 'X')
+board.set_point(1, 0, ' ')
 board.set_point(1, 1, 'X')
-board.set_point(1, 2, 'X')
+board.set_point(1, 2, ' ')
 board.set_point(2, 0, 'X')
-board.set_point(2, 1, 'X')
-board.set_point(2, 2, 'X')
+board.set_point(2, 1, ' ')
+board.set_point(2, 2, ' ')
+
+board.draw_board()
 
 print()
 
-board.draw_board()
+print(board.is_over())
