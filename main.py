b46db16cdca07b967d8b54dff93b672fb38ae08c
kamilbizon <kamilbizon@outlook.com>
2018-03-23 02:20:55 +0100

    pretty main

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
 from game import Game
 
-# todo: handle inputs
-# todo: base game loop and player prompts
-# todo: randomizing who goes first and attributing win accordingly
-
 
 def main():
     game = Game()
     game.start_game()
-
-# board = tttBoard()
-#
-# board.set_point(0, 0, ' ')
-# board.set_point(0, 1, ' ')
-# board.set_point(0, 2, 'X')
-# board.set_point(1, 0, ' ')
-# board.set_point(1, 1, 'X')
-# board.set_point(1, 2, ' ')
-# board.set_point(2, 0, 'X')
-# board.set_point(2, 1, ' ')
-# board.set_point(2, 2, ' ')
-#
-# print(board.get_boardstate())
-#
-# print()
-#
-# print(board.is_over())
 
 
 if __name__ == '__main__':
