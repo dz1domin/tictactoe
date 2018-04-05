b057ab4a00fcd4ef0c370cdc6ab5ed3109f4d2e5
dz1domin <36998838+dz1domin@users.noreply.github.com>
2018-04-05 16:22:05 +0200

    added some files that probably need to be added

--- TicTacToeServer.py
+++ TicTacToeServer.py
@@ -9,10 +9,17 @@ import socket
 def main():
     TCP_IP = '127.0.0.1'
     TCP_PORT = 5005
-    BUFFER_SIZE = 1024
-
+    BUFFER_SIZE = 512
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    player_list = [TicTacToePlayers() for _ in range(2)]
+    s.bind((TCP_IP, TCP_PORT))
+    s.listen(1)
+    player_list = []
+
+    while len(player_list) < 2:
+        conn, addr = s.accept()
+        player_list.append(TicTacToePlayers(addr, TCP_PORT, 'Player' + str(len(player_list)), conn))
+        data = conn.recv(BUFFER_SIZE)
+        print(addr, conn, data)
 
 
 if __name__ == '__main__':
